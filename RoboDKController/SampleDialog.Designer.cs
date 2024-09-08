dc2ad9aca79771be6e70f11aea2e5aa6cfbd9583
Arman Abdollahi <la_rq@yahoo.com>
2024-09-08 23:46:08 +0330

    First Commit

---
+++ RoboDKController/SampleDialog.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SamplePanelRoboDK
+{
+    partial class SampleDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlControl = new System.Windows.Forms.Panel();
+            this.chkRunMode = new System.Windows.Forms.CheckBox();
+            this.btnMoveToHomePos = new System.Windows.Forms.Button();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.btnMoveRobotPos = new System.Windows.Forms.Button();
+            this.txtJoints = new System.Windows.Forms.TextBox();
+            this.btnMoveRobotJoint = new System.Windows.Forms.Button();
+            this.pnlRDK = new System.Windows.Forms.Panel();
+            this.btnReadRobotPos = new System.Windows.Forms.Button();
+            this.btnReadRobotJoint = new System.Windows.Forms.Button();
+            this.txtRobotIP = new System.Windows.Forms.TextBox();
+            this.btnConnectToRobot = new System.Windows.Forms.Button();
+            this.txtRobotPort = new System.Windows.Forms.TextBox();
+            this.pnlControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlControl
+            // 
+            this.pnlControl.BackColor = System.Drawing.Color.Black;
+            this.pnlControl.Controls.Add(this.txtRobotPort);
+            this.pnlControl.Controls.Add(this.txtRobotIP);
+            this.pnlControl.Controls.Add(this.btnConnectToRobot);
+            this.pnlControl.Controls.Add(this.btnReadRobotPos);
+            this.pnlControl.Controls.Add(this.btnReadRobotJoint);
+            this.pnlControl.Controls.Add(this.chkRunMode);
+            this.pnlControl.Controls.Add(this.btnMoveToHomePos);
+            this.pnlControl.Controls.Add(this.txtPosition);
+            this.pnlControl.Controls.Add(this.btnMoveRobotPos);
+            this.pnlControl.Controls.Add(this.txtJoints);
+            this.pnlControl.Controls.Add(this.btnMoveRobotJoint);
+            this.pnlControl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlControl.Location = new System.Drawing.Point(0, 0);
+            this.pnlControl.Name = "pnlControl";
+            this.pnlControl.Size = new System.Drawing.Size(1217, 87);
+            this.pnlControl.TabIndex = 0;
+            // 
+            // chkRunMode
+            // 
+            this.chkRunMode.AutoSize = true;
+            this.chkRunMode.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.chkRunMode.Location = new System.Drawing.Point(1092, 55);
+            this.chkRunMode.Name = "chkRunMode";
+            this.chkRunMode.Size = new System.Drawing.Size(96, 17);
+            this.chkRunMode.TabIndex = 9;
+            this.chkRunMode.Text = "Apply to Robot";
+            this.chkRunMode.UseVisualStyleBackColor = true;
+            this.chkRunMode.CheckedChanged += new System.EventHandler(this.chkRunMode_CheckedChanged);
+            // 
+            // btnMoveToHomePos
+            // 
+            this.btnMoveToHomePos.Location = new System.Drawing.Point(1092, 12);
+            this.btnMoveToHomePos.Name = "btnMoveToHomePos";
+            this.btnMoveToHomePos.Size = new System.Drawing.Size(100, 23);
+            this.btnMoveToHomePos.TabIndex = 8;
+            this.btnMoveToHomePos.Text = "Home";
+            this.btnMoveToHomePos.UseVisualStyleBackColor = true;
+            this.btnMoveToHomePos.Click += new System.EventHandler(this.btnMoveToHomePos_Click);
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPosition.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtPosition.Location = new System.Drawing.Point(12, 42);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(269, 29);
+            this.txtPosition.TabIndex = 7;
+            // 
+            // btnMoveRobotPos
+            // 
+            this.btnMoveRobotPos.Location = new System.Drawing.Point(287, 42);
+            this.btnMoveRobotPos.Name = "btnMoveRobotPos";
+            this.btnMoveRobotPos.Size = new System.Drawing.Size(128, 29);
+            this.btnMoveRobotPos.TabIndex = 6;
+            this.btnMoveRobotPos.Text = "Move Robot Pos";
+            this.btnMoveRobotPos.UseVisualStyleBackColor = true;
+            this.btnMoveRobotPos.Click += new System.EventHandler(this.btnMoveRobotPos_Click);
+            // 
+            // txtJoints
+            // 
+            this.txtJoints.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtJoints.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtJoints.Location = new System.Drawing.Point(12, 12);
+            this.txtJoints.Name = "txtJoints";
+            this.txtJoints.Size = new System.Drawing.Size(269, 29);
+            this.txtJoints.TabIndex = 5;
+            // 
+            // btnMoveRobotJoint
+            // 
+            this.btnMoveRobotJoint.Location = new System.Drawing.Point(287, 12);
+            this.btnMoveRobotJoint.Name = "btnMoveRobotJoint";
+            this.btnMoveRobotJoint.Size = new System.Drawing.Size(128, 29);
+            this.btnMoveRobotJoint.TabIndex = 4;
+            this.btnMoveRobotJoint.Text = "Move Robot Joint";
+            this.btnMoveRobotJoint.UseVisualStyleBackColor = true;
+            this.btnMoveRobotJoint.Click += new System.EventHandler(this.btnMoveRobotJoint_Click);
+            // 
+            // pnlRDK
+            // 
+            this.pnlRDK.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlRDK.Location = new System.Drawing.Point(0, 87);
+            this.pnlRDK.Name = "pnlRDK";
+            this.pnlRDK.Size = new System.Drawing.Size(1217, 588);
+            this.pnlRDK.TabIndex = 0;
+            // 
+            // btnReadRobotPos
+            // 
+            this.btnReadRobotPos.Location = new System.Drawing.Point(421, 42);
+            this.btnReadRobotPos.Name = "btnReadRobotPos";
+            this.btnReadRobotPos.Size = new System.Drawing.Size(43, 29);
+            this.btnReadRobotPos.TabIndex = 11;
+            this.btnReadRobotPos.Text = "R";
+            this.btnReadRobotPos.UseVisualStyleBackColor = true;
+            this.btnReadRobotPos.Click += new System.EventHandler(this.btnReadRobotPos_Click);
+            // 
+            // btnReadRobotJoint
+            // 
+            this.btnReadRobotJoint.Location = new System.Drawing.Point(421, 12);
+            this.btnReadRobotJoint.Name = "btnReadRobotJoint";
+            this.btnReadRobotJoint.Size = new System.Drawing.Size(43, 29);
+            this.btnReadRobotJoint.TabIndex = 10;
+            this.btnReadRobotJoint.Text = "R";
+            this.btnReadRobotJoint.UseVisualStyleBackColor = true;
+            this.btnReadRobotJoint.Click += new System.EventHandler(this.btnReadRobotJoint_Click);
+            // 
+            // txtRobotIP
+            // 
+            this.txtRobotIP.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRobotIP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtRobotIP.Location = new System.Drawing.Point(488, 11);
+            this.txtRobotIP.Name = "txtRobotIP";
+            this.txtRobotIP.Size = new System.Drawing.Size(115, 29);
+            this.txtRobotIP.TabIndex = 13;
+            this.txtRobotIP.Text = "10.20.30.40";
+            // 
+            // btnConnectToRobot
+            // 
+            this.btnConnectToRobot.Location = new System.Drawing.Point(683, 11);
+            this.btnConnectToRobot.Name = "btnConnectToRobot";
+            this.btnConnectToRobot.Size = new System.Drawing.Size(128, 29);
+            this.btnConnectToRobot.TabIndex = 12;
+            this.btnConnectToRobot.Text = "Connect to Robot";
+            this.btnConnectToRobot.UseVisualStyleBackColor = true;
+            this.btnConnectToRobot.Click += new System.EventHandler(this.btnConnectToRobot_Click);
+            // 
+            // txtRobotPort
+            // 
+            this.txtRobotPort.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRobotPort.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtRobotPort.Location = new System.Drawing.Point(609, 11);
+            this.txtRobotPort.Name = "txtRobotPort";
+            this.txtRobotPort.Size = new System.Drawing.Size(68, 29);
+            this.txtRobotPort.TabIndex = 14;
+            this.txtRobotPort.Text = "1234";
+            // 
+            // SampleDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 675);
+            this.Controls.Add(this.pnlRDK);
+            this.Controls.Add(this.pnlControl);
+            this.Name = "SampleDialog";
+            this.ShowIcon = false;
+            this.Text = "KUKA Control Dialog";
+            this.pnlControl.ResumeLayout(false);
+            this.pnlControl.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlControl;
+        private System.Windows.Forms.Panel pnlRDK;
+        private System.Windows.Forms.Button btnMoveRobotJoint;
+        private System.Windows.Forms.TextBox txtJoints;
+        private System.Windows.Forms.Button btnMoveRobotPos;
+        private System.Windows.Forms.TextBox txtPosition;
+        private System.Windows.Forms.Button btnMoveToHomePos;
+        private System.Windows.Forms.CheckBox chkRunMode;
+        private System.Windows.Forms.Button btnReadRobotPos;
+        private System.Windows.Forms.Button btnReadRobotJoint;
+        private System.Windows.Forms.TextBox txtRobotPort;
+        private System.Windows.Forms.TextBox txtRobotIP;
+        private System.Windows.Forms.Button btnConnectToRobot;
+    }
+}
---
+++ RoboDKController/SampleDialog.cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RoboDk.API;
+using RoboDk.API.Exceptions;
+using RoboDk.API.Model;
+
+namespace SamplePanelRoboDK
+{
+    public partial class SampleDialog : Form
+    {
+        // Define if the robot movements will be blocking
+        private const bool MoveBlocking = false;
+
+        private SpashDialog spashDialog;
+
+        // RDK holds the main object to interact with RoboDK.
+        // The RoboDK application starts when a RoboDK object is created.
+        private IRoboDK _rdk;
+
+        // Keep the ROBOT item as a global variable
+        private IItem _robot;
+
+
+        public SampleDialog()
+        {
+            InitializeComponent();
+
+            CheckForIllegalCrossThreadCalls = false;
+
+            this.pnlRDK.Resize += this.Panel_Resized;
+
+            this.FormClosed += this.SampleDialog_FormClosed;
+
+            this.Load += SampleDialog_Load;
+            this.Shown += SampleDialog_Shown;
+        }
+
+        private void SampleDialog_Shown(object sender, EventArgs e)
+        {
+
+            EmbedRoboDK();
+
+            _rdk.ShowRoboDK();
+
+            LoadKukaRobotFile();
+
+            //Hide roboDK internal features:
+            //LockRoboDK();
+
+            //Active 3d viewport only:
+            //_rdk.SetWindowFlags(WindowFlags.View3DActive);
+
+            //Activate all features:
+            _rdk.SetWindowFlags(WindowFlags.All);
+
+            RetreiveJoints();
+
+            HideSplashScreen();
+
+            _rdk.SetSimulationSpeed(1);
+
+        }
+
+        private void HideSplashScreen()
+        {
+            this.spashDialog.Close();
+        }
+
+        private void SampleDialog_Load(object sender, EventArgs e)
+        {
+            ShowSplashScreen();
+
+            OpenRoboDK();
+        }
+
+        private void ShowSplashScreen()
+        {
+            this.spashDialog = new SpashDialog();
+
+            spashDialog.Show();
+            Application.DoEvents();
+        }
+
+        private void OpenRoboDK()
+        {
+            if (!IsRoboDKReady())
+            {
+                //or let rdk to use exists instance
+                KillOldRoboDKProcess();
+
+                var roboDK = new RoboDK(" -NOSPLASH -HIDDEN -ColorBgBottom=black -ColorBgTop=black ");
+                
+                //if (IsRoboDKAlreadyRunning())
+                //{
+                //    roboDK.StartNewInstance = false;
+                //}
+
+                _rdk = roboDK;
+
+                _rdk.ShowRoboDK();
+            }
+        }
+
+        private bool IsRoboDKAlreadyRunning()
+        {
+            return Process.GetProcessesByName("RoboDK").Any();
+        }
+
+        private void KillOldRoboDKProcess()
+        {
+            var p = Process.GetProcessesByName("RoboDK").FirstOrDefault();
+
+            if (p != null)
+            {
+                p.Kill();
+                p.WaitForExit();
+            }
+        }
+
+        public bool IsRoboDKReady()
+        {
+            // check if the RDK object has been initialized:
+            if (_rdk == null)
+            {
+                return false;
+            }
+
+            // Check if the RDK API is connected
+            if (!_rdk.Connected())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ConnectToRoboDK()
+        {
+            _rdk.Connect();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        private void EmbedRoboDK()
+        {
+            // hook window pointer to the integrated panel
+            //SetParent(RDK.PROCESS.MainWindowHandle, panel_rdk.Handle);
+            SetParent(_rdk.GetWindowHandle(), pnlRDK.Handle);
+
+            //RDK.SetWindowState(RoboDk.API.Model.WindowState.Show); // show RoboDK window if it was hidden
+            _rdk.SetWindowState(RoboDk.API.Model.WindowState
+                .Cinema); // sets cinema mode (remove the menu, the toolbar, the title bar and the status bar)
+            _rdk.SetWindowState(RoboDk.API.Model.WindowState.Fullscreen); // maximizes the screen and sets cinema mode
+
+            // make form height larger
+            Size = new Size(Size.Width, 700);
+
+
+        }
+
+        //load KUKA KRC2 3D model from bi dir
+        private void LoadKukaRobotFile()
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "KUKAKRC2102.rdk");
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"Kuka file not found in {filePath}");
+            }
+
+            var item = _rdk.AddFile(filePath);
+
+            if (item.Valid())
+            {
+                SelectRobot();
+            }
+            else
+            {
+                ShowError($@"Could not load: {filePath}");
+            }
+
+        }
+
+        /// <summary>
+        ///     Update the ROBOT variable by choosing the robot available in the currently open station
+        ///     If more than one robot is available, a popup will be displayed
+        /// </summary>
+        public void SelectRobot()
+        {
+            _robot = _rdk.ItemUserPick("Select a robot", ItemType.Robot); // select robot among available robots
+            //ROBOT = RL.getItem("ABB IRB120", ITEM_TYPE_ROBOT); // select by name
+            //ITEM = RL.ItemUserPick("Select an item"); // Select any item in the station
+            if (_robot.Valid())
+            {
+                _robot.NewLink(); // This will create a new communication link (another instance of the RoboDK API), this is useful if we are moving 2 robots at the same time.                
+            }
+            else
+            {
+                ShowError(@"Could not select robot. Robot is not available, Load a file first");
+            }
+        }
+
+        private void ShowError(string message)
+        {
+            MessageBox.Show(message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        private void Panel_Resized(object sender, EventArgs e)
+        {
+            // resize the content of the panel_rdk when it is resized
+            MoveWindow(_rdk.GetWindowHandle(), 0, 0, pnlRDK.Width, pnlRDK.Height, true);
+        }
+
+        private void SampleDialog_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Force to stop and close RoboDK (optional)
+            // RDK.CloseAllStations(); // close all stations
+            // RDK.Save("path_to_save.rdk"); // save the project if desired
+            _rdk.CloseRoboDK();
+            _rdk = null;
+        }
+
+        //lock internal robodk feaures
+        private void LockRoboDK()
+        {
+            //RDK.setFlagsRoboDK(RoboDK.FLAG_ROBODK_MENUEDIT_ACTIVE | RoboDK.FLAG_ROBODK_MENUEDIT_ACTIVE);
+            _rdk.SetWindowFlags(WindowFlags.None);
+            _rdk.SetItemFlags(ItemFlags.None);
+            if (_robot != null && _robot.Valid()) _robot.SetItemFlags();
+        }
+
+        //unlock internal robodk feaures
+        private void UnlockRoboDK()
+        {
+            _rdk.SetWindowFlags(WindowFlags.All);
+            _rdk.SetItemFlags();
+            _rdk.ShowRoboDK();
+
+        }
+
+        //get current pos of joints
+        private void RetreiveJoints()
+        {
+
+            var joints = _robot.Joints();
+            var pose = _robot.Pose();
+
+            // update the joints
+            var strjoints = Values_2_String(joints);
+            txtJoints.Text = strjoints;
+
+            // update the pose as xyzwpr
+            var xyzwpr = pose.ToTxyzRxyz();
+            var strpose = Values_2_String(xyzwpr);
+            txtPosition.Text = strpose;
+        }
+
+        private void btnMoveRobotJoint_Click(object sender, EventArgs e)
+        {
+            // retrieve the robot joints from the text and validate input
+            var joints = String_2_Values(txtJoints.Text);
+
+            // make sure RDK is running and we have a valid input
+            if (joints == null) return;
+
+            try
+            {
+                //bool jnts_valid = ROBOT.setJoints(joints, RoboDK.SETJOINTS_SATURATE_APPLY);
+                //Console.WriteLine("Robot joints are valid: " + jnts_valid.ToString());
+                _robot.MoveL(joints, MoveBlocking);
+            }
+            catch (RdkException rdkException)
+            {
+                ShowError("The robot can't move to " + txtJoints.Text + Environment.NewLine + rdkException.Message);
+            }
+        }
+
+        private void btnMoveRobotPos_Click(object sender, EventArgs e)
+        {
+            // retrieve the robot position from the text and validate input
+            var xyzwpr = String_2_Values(txtPosition.Text);
+
+            // make sure RDK is running and we have a valid input
+            if (xyzwpr == null) return;
+
+            //Mat pose = Mat.FromXYZRPW(xyzwpr);
+            var pose = Mat.FromTxyzRxyz(xyzwpr);
+            try
+            {
+                _robot.MoveL(pose, MoveBlocking);
+            }
+            catch (RdkException rdkex)
+            {
+                ShowError("The robot can't move to " + txtPosition.Text + Environment.NewLine + rdkex.Message);
+            }
+        }
+
+        /// <summary>
+        ///     Convert a list of numbers provided as a string to an array of values
+        /// </summary>
+        /// <param name="strvalues"></param>
+        /// <returns></returns>
+        public double[] String_2_Values(string strvalues)
+        {
+            double[] dvalues = null;
+            try
+            {
+                dvalues = Array.ConvertAll(strvalues.Split(','), double.Parse);
+            }
+            catch (FormatException ex)
+            {
+                ShowError($@"Invalid input: {strvalues}: {ex.Message}");
+            }
+
+            return dvalues;
+        }
+
+        /// <summary>
+        ///     Convert an array of values as a string
+        /// </summary>
+        /// <param name="dvalues"></param>
+        /// <returns></returns>
+        public string Values_2_String(double[] dvalues)
+        {
+            if (dvalues == null || dvalues.Length < 1) return "Invalid values";
+            // Not supported on .NET Framework 2.0:
+            //string strvalues = String.Join(" , ", dvalues.Select(p => p.ToString("0.0")).ToArray());
+            var strvalues = dvalues[0].ToString("0.0");
+            for (var i = 1; i < dvalues.Length; i++) strvalues += $" , {dvalues[i]:0.0}";
+
+            return strvalues;
+            //return "";
+        }
+
+        private void btnMoveToHomePos_Click(object sender, EventArgs e)
+        {
+            var jointsHome = _robot.JointsHome();
+
+            _robot.MoveJ(jointsHome);
+        }
+
+        private void chkRunMode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkRunMode.Tag != null)
+            {
+                return;
+            }
+
+            chkRunMode.Tag = "processing";
+
+            if (chkRunMode.Checked)
+            {
+                //run on robot:
+
+                // Important: stop any previous program generation (if we selected offline programming mode)
+                _rdk.Disconnect();
+
+                // Connect to real robot
+                if (_robot.Connect())
+                {
+                    // Set to Run on Robot robot mode:
+                    _rdk.SetRunMode(RunMode.RunRobot);
+                }
+                else
+                {
+                    ShowError(@"Can't connect to the robot. Check connection and parameters.");
+
+                    chkRunMode.Checked = false;
+                }
+            }
+            else
+            {
+                //run in simulation:
+
+                // Important: stop any previous program generation (if we selected offline programming mode)
+                _rdk.Disconnect();
+                
+                // Set to simulation mode:
+                _rdk.SetRunMode();
+            }
+
+            chkRunMode.Tag = null;
+        }
+
+        private void btnReadRobotJoint_Click(object sender, EventArgs e)
+        {
+            var joints = _robot.Joints();
+            // update the joints
+            var strjoints = Values_2_String(joints);
+            txtJoints.Text = strjoints;
+        }
+
+        private void btnReadRobotPos_Click(object sender, EventArgs e)
+        {
+            // update the pose as xyzwpr
+            var pose = _robot.Pose();
+            var xyzwpr = pose.ToTxyzRxyz();
+            var strpose = Values_2_String(xyzwpr);
+            txtPosition.Text = strpose;
+        }
+
+        private void btnConnectToRobot_Click(object sender, EventArgs e)
+        {
+            ConfigureKukaRobot();
+            
+            ConnectToRobot();
+        }
+
+        private void ConfigureKukaRobot()
+        {
+            try
+            {
+                _robot.setConnectionParams(txtRobotIP.Text, int.Parse(txtRobotPort.Text), "C:\\", "morteza", "123");
+            }
+            catch (Exception ex)
+            {
+                ShowError("Unable to set the robot connection params" + Environment.NewLine + ex.Message);
+            }
+        }
+
+        private void ConnectToRobot()
+        {
+            try
+            {
+                if (_robot.Connect(txtRobotIP.Text))
+                {
+                    MessageBox.Show("Connected");
+                }
+                else
+                {
+                    var parameters = _robot.ConnectionParams();
+
+                    ShowError($"Unable to connect to the robot\nIpAddress: {parameters.RobotIp}\nPort: {parameters.Port}");
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+            }
+        }
+
+    }
+}
