f0ccddcc49b41982789d1a3d06c80ebd22e822c3
Arman Abdollahi <la_rq@yahoo.com>
2024-09-11 18:35:04 +0330

    Removed unnessacery usings

--- RoboDKController/SampleDialog.cs
+++ RoboDKController/SampleDialog.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.ComTypes;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using RoboDk.API;
 using RoboDk.API.Exceptions;
--- RoboDkApi/Model/RobotConnectionType.cs
+++ RoboDkApi/Model/RobotConnectionType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RoboDk.API.Model
+﻿namespace RoboDk.API.Model
 {
     public enum RobotConnectionType
     {
